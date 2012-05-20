a727d738e2b35b5aa6c4e1854d4be8c16529080f
Lorenzo Urbini <superluglor@gmail.com>
2012-05-20 22:39:57 +0100

    old files wipe out

--- tests/features/security_steps.py
+++
@@ -1,6 +0,0 @@
-from lettuce import step
-
-@step('When I try the most simple sql injection attack')
-def when_i_try_the_most_simple_sql_injection_attack(step):
-  step.given('I log into the admin account')
-
